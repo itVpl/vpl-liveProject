c660a174b193b8a508a601bb3916e062b18ca934
itVpl <it@vpower-logistics.com>
2025-07-22 14:03:25 +0530

    shipper_driverController.js cookie configuration code updated

--- controllers/shipper_driverController.js
+++ controllers/shipper_driverController.js
@@ -215,8 +215,8 @@ const loginUser = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-            sameSite: 'None',
-            secure: true,
+            // sameSite: 'None',
+            // secure: true,
         });
 
         res.status(200).json({
